19c35f1693d63ff0cfe305a10ede2f7f835c8f47
Daigo Tanaka <daigo.tanaka@gmail.com>
2020-07-28 18:21:58 +0000

    change current, archive to last, runs

--- handoff/config.py
+++ handoff/config.py
@@ -3,5 +3,5 @@ CONFIG_DIR = "config"
 FILES_DIR = "files"
 PROJECT_FILE = "project.yml"
 
-BUCKET_CURRENT_PREFIX = "current"
-BUCKET_ARCHIVE_PREFIX = "archive"
+BUCKET_CURRENT_PREFIX = "last"
+BUCKET_ARCHIVE_PREFIX = "runs"

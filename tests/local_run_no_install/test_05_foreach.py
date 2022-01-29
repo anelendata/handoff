1b4fd25c9a6d049b2f56431a48cb46b821b3e179
Daigo Tanaka <daigo.tanaka@gmail.com>
2022-01-28 21:09:11 -0800

    fix: fix std out timing issue in the tests

--- tests/local_run_no_install/test_05_foreach.py
+++ tests/local_run_no_install/test_05_foreach.py
@@ -17,10 +17,15 @@ def test_05_foreach():
                    container_provider="docker",
                    stage="prod",
                    push_artifacts=False)
-        with open(os.path.join(workspace_dir, ARTIFACTS_DIR,
-                               "verify_result_stdout.log")) as f:
-            s = f.readline()
-            assert int(s.strip("\n").strip()) == 5
+        retries = 5
+        s = None
+        while retries > 0:
+            with open(os.path.join(workspace_dir, ARTIFACTS_DIR,
+                                   "verify_result_stdout.log"), "r") as f:
+                s = f.readline()
+            if s and len(s) > 0:
+                break
+        assert int(s.strip("\n").strip()) == 5
 
 
 def test_05a_foreach_no_kill_on_fail():
@@ -35,8 +40,13 @@ def test_05a_foreach_no_kill_on_fail():
                    container_provider="docker",
                    stage="prod",
                    push_artifacts=False)
-        with open(os.path.join(workspace_dir, ARTIFACTS_DIR,
-                               "verify_result_stdout.log")) as f:
-            s = f.readline()
-            # The output line sums to 4 with 1, 2, 3, 5 (skipping the fail on 4)
-            assert int(s.strip("\n").strip()) == 4
+        retries = 5
+        s = None
+        while retries > 0:
+            with open(os.path.join(workspace_dir, ARTIFACTS_DIR,
+                                   "verify_result_stdout.log"), "r") as f:
+                s = f.readline()
+            if s and len(s) > 0:
+                break
+        # The output line sums to 4 with 1, 2, 3, 5 (skipping the fail on 4)
+        assert int(s.strip("\n").strip()) == 4
